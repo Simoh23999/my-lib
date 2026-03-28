eff483d3fe19b006dd89bbe086489a0fa8ad9bc0
simo <152016679+Simoh23999@users.noreply.github.com>
2026-03-28 17:49:30 +0100

    Refactor error function to remove script parameter

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,7 +6,7 @@ def call(name = "sall") {
 	echo "you call me, I answer !!, my name is ${name}"
 }
 
-def error(Map config = [name:"simo", age: 18], script){
+def error(Map config = [name:"simo", age: 18]){
 	echo "${config.name} called my defore, his age is ${config.age}"
-	script.sh "ls -al"
+	sh "ls -al"
 }
