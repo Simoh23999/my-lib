3d5b467a7152ec7668ad4b887ae166d5e4f77d01
simo <152016679+Simoh23999@users.noreply.github.com>
2026-03-28 16:41:19 +0100

    Add error function to log.groovy
    Added error function to log.groovy for error handling.

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,3 +5,8 @@ def info(String msg) {
 def call(name = "sall") {
 	echo "you call me, I answer !!, my name is ${name}"
 }
+
+def error(Map config, script){
+	echo "${config.name} called my defore"
+	script.sh "ls -al"
+}
