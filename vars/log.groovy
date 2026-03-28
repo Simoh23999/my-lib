4b9ec2d4708edd5a1b8f2c3c929d6d34fe770de8
simo <152016679+Simoh23999@users.noreply.github.com>
2026-03-28 16:51:11 +0100

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,7 +6,7 @@ def call(name = "sall") {
 	echo "you call me, I answer !!, my name is ${name}"
 }
 
-def error(Map config, script){
+def error(Map config = [name:"simo"], script){
 	echo "${config.name} called my defore"
 	script.sh "ls -al"
 }
